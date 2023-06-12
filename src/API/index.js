f9dd1f595c138485f3b20ab84073ab50a5820807
Azmenezi <azmenezi@gmail.com>
2023-06-12 22:03:31 +0300

    auth and index api

--- src/API/index.js
+++ src/API/index.js
@@ -3,13 +3,15 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://coded-projects-api.herokuapp.com/api/bank/v3",
 });
-
+const authInstance = axios.create({
+  baseURL: "https://coded-projects-api.herokuapp.com/api/auth/v3",
+});
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  const access = localStorage.getItem("access");
+  if (access) {
+    config.headers.Authorization = `Bearer ${access}`;
   }
   return config;
 });
 
-export default instance;
+export { authInstance, instance };
